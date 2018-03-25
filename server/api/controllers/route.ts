1ce83ab98da202bef74815e0672108805898cc5e
amarouche <amarou_o@etna-alternance.net>
2018-03-25 12:56:03 +0200

    2

--- server/api/controllers/route.ts
+++ server/api/controllers/route.ts
@@ -4,4 +4,4 @@ export default express.Router()
     //.post('/domains.:type', controller.create)
     // .get('/', domainsController.all)
     .get('/domains.:type', domainsController.all)
-    .get('/domains/mailer.:type', domainsController.mailer);
+    .get('/domains/:mailer.:type', domainsController.mailer);
